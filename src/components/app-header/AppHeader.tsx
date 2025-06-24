81a85169b3ae731d1bec7b6242c4a42333c11cfd
neilnicky <neilnicholaskanat@gmail.com>
2025-06-24 12:10:52 +0530

    feat: create main application header

---
+++ src/components/app-header/AppHeader.tsx
@@ -0,0 +1,19 @@
+import Cart from "../Cart";
+import HeaderOptions from "./HeaderOptions";
+import HeaderProfile from "./HeaderProfile";
+import SearchBar from "./SearchBar";
+
+export default function AppHeader() {
+  return (
+    <>
+      <header className="bg-card shadow-sm py-2 px-4 fixed top-0 left-0 right-0 z-40 flex items-center justify-between gap-2 sm:gap-4 border-b border-border">
+        <HeaderOptions />
+        <SearchBar />
+        <div className="flex items-center gap-1 sm:gap-2 flex-shrink-0">
+          <Cart />
+          <HeaderProfile />
+        </div>
+      </header>
+    </>
+  );
+}
