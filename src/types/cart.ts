449c57d3521f5b0d501312b90e8c4385688d2072
neilnicky <neilnicholaskanat@gmail.com>
2025-06-28 22:59:01 +0530

    refactor: modify cartItem type

--- src/types/cart.ts
+++ src/types/cart.ts
@@ -1,3 +1,8 @@
-export interface CartItems {
-  [productId: string]: number;
+// export interface CartItems {
+//   [productId: string]: number;
+// }
+
+export interface CartItem {
+  productId: string;
+  quantity: number;
 }
