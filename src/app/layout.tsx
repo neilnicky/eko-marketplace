7ff186795d89c3a5834c643b6eb9f21e44a269fc
neilnicky <neilnicholaskanat@gmail.com>
2025-06-28 23:09:46 +0530

    feat: wrapped layout with QUeryClientProvider for now

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,14 @@
+"use client"
+
 import AppHeader from "@/components/common/AppHeader";
 import "./globals.css";
 import BottomNavBar from "@/components/common/BottomNavBar";
 import { CreateFab } from "@/components/common/CreateFab";
 import StoreProvider from "./StoreProvider";
 import { preventFoucScript } from "@/lib/scripts/preventFouc";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 export default function RootLayout({
   children,
@@ -13,16 +18,18 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={`antialiased bg-background`}>
-        <StoreProvider>
-          <AppHeader />
-          <div className="mt-14">{children}</div>
-          <BottomNavBar />
-          <CreateFab />
-        </StoreProvider>
-        <script dangerouslySetInnerHTML={{ __html: preventFoucScript }} />
-        {/* <Script id="prevent-fouc" strategy="beforeInteractive">
+        <QueryClientProvider client={queryClient}>
+          <StoreProvider>
+            <AppHeader />
+            <div className="mt-14">{children}</div>
+            <BottomNavBar />
+            <CreateFab />
+          </StoreProvider>
+          <script dangerouslySetInnerHTML={{ __html: preventFoucScript }} />
+          {/* <Script id="prevent-fouc" strategy="beforeInteractive">
           {preventFoucScript}
-        </Script> */}
+          </Script> */}
+        </QueryClientProvider>
       </body>
     </html>
   );
