16fdf397a1a42b7f8d6b305bd0918b504287ac0e
neilnicky <neilnicholaskanat@gmail.com>
2025-06-30 22:06:14 +0530

    feat:  create sorting select component

--- src/components/marketplace/SortingSelect.tsx
+++ src/components/marketplace/SortingSelect.tsx
@@ -1,7 +1,31 @@
-import React from 'react'
+import React, { useState } from "react";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "../ui/select";
+import { useUserLocation } from "@/hooks/useUserLocation";
 
 export default function SortingSelect() {
+  const [sorting, setSorting] = useState("newest");
+  const { location } = useUserLocation();
+
   return (
-    <div>SortingSelect</div>
-  )
+    <div className="px-2 sm:px-4">
+      <Select value={sorting} onValueChange={setSorting}>
+        <SelectTrigger className="w-full md:w-48 text-xs">
+          <SelectValue placeholder="Sort by" />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectItem value="newest">Newest</SelectItem>
+          <SelectItem value="price_asc">Lowest Price</SelectItem>
+          <SelectItem value="price_desc">Highest Price</SelectItem>
+          <SelectItem value="best_rated">Best Rated</SelectItem>
+          {location?.city && <SelectItem value="closest">Closest</SelectItem>}
+        </SelectContent>
+      </Select>
+    </div>
+  );
 }
