3ba08baa121ab731d8e084687c5c9ac3166f0ddf
neilnicky <neilnicholaskanat@gmail.com>
2025-07-01 13:43:42 +0530

    feat: create cart footere

--- src/components/marketplace/CartFooter.tsx
+++ src/components/marketplace/CartFooter.tsx
@@ -1,7 +1,24 @@
-import React from 'react'
+import { useAppSelector } from "@/hooks/reduxHooks";
+import { RootState } from "@/store/store";
+import Link from "next/link";
 
 export default function CartFooter() {
+  const { totalItems, totalPrice } = useAppSelector(
+    (state: RootState) => state.cart
+  );
+  if (totalItems === 0) return null;
+
   return (
-    <div>CartFooter</div>
-  )
+    <div className="fixed bottom-[70px] left-0 right-0 z-30 p-4 flex justify-center">
+      <div className="w-full max-w-md">
+        <Link
+          href="/cart"
+          className="w-full bg-primary hover:bg-primary/90 text-primary-foreground shadow-lg text-base h-12 flex justify-between items-center px-6"
+        >
+          <span>View Cart ({totalItems})</span>
+          <span>R$ {totalPrice.toFixed(2)}</span>
+        </Link>
+      </div>
+    </div>
+  );
 }
