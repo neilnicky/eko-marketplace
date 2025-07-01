85e14bba1784304e0397451305c6173b10ae4b21
neilnicky <neilnicholaskanat@gmail.com>
2025-07-01 13:44:11 +0530

    feat: create dedicated product product page

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -1,7 +1,30 @@
-import React from 'react'
+"use client";
 
-export default function page() {
+import { useProduct } from "@/hooks/useProducts";
+import Image from "next/image";
+import { useParams } from "next/navigation";
+import React from "react";
+
+export default function ProductPage() {
+  const { id } = useParams<{ id: string }>();
+  const { data: product, isLoading, error } = useProduct(id);
+
+  if (isLoading) {
+    return <div>Loading product...</div>;
+  }
+  if (error) {
+    return <div>Error loading product</div>;
+  }
+
   return (
-    <div>page</div>
-  )
+    <div>
+      <Image
+        alt={product?.name ?? ""}
+        src={product?.image_urls[0] ?? "/assets/ekomart_logo.png"}
+        width={400}
+        height={400}
+      />
+      <div className="text-4xl">{product?.name}</div>
+    </div>
+  );
 }
